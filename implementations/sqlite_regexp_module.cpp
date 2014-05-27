8fcba58d88745af20fffe8812ad12ea6dede8004
Ben Firner <bfirner@eden.rutgers.edu>
2014-05-27 17:57:38 -0400

    Improved sqlite3 REGEXP implementation.
    This implementation may avoid recompiling expression.
    
    	modified:   implementations/CMakeLists.txt
    	new file:   implementations/regex_store.cpp
    	new file:   implementations/regex_store.hpp
    	modified:   implementations/sqlite_regexp_module.cpp
    	modified:   implementations/sqlite_regexp_module.hpp

--- implementations/sqlite_regexp_module.cpp
+++ implementations/sqlite_regexp_module.cpp
@@ -1,55 +1,74 @@
+/*
+ * Copyright (c) 2014 Bernhard Firner
+ * All rights reserved.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA
+ * or visit http://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+/*******************************************************************************
+ * REGEXP function implementation for the sqlite3 database.
+ ******************************************************************************/
+
 #include "sqlite_regexp_module.hpp"
+#include "regex_store.hpp"
 
 #include <string>
 
 #include <sqlite3.h>
 
-//TODO In the future C++11 support for regex should be used over these POSIX
-//regex c headers.
-#include <sys/types.h>
-#include <regex.h>
-
 using std::string;
 using std::u16string;
 
+//Destroy the RegexStore object when the database is closed
+static void xDestroy(void* userParam) {
+	RegexStore* myRegStore = static_cast<RegexStore*>(userParam);
+	delete myRegStore;
+}
+
 //Callback that implements regular expressions in sqlite3
 static void sqlite3_regexp(sqlite3_context *context, int argc, sqlite3_value **argv) {
+	//Function expects 2 arguments
   if (argc != 2) {
     sqlite3_result_error_code(context, 1);
     return;
   }
 
+	//If the inputs aren't text then this function won't work
   if ( (sqlite3_value_type(argv[0]) != SQLITE_TEXT )
       or ((sqlite3_value_type(argv[1]) != SQLITE_TEXT ))) {
     sqlite3_result_error_code(context, 2);
     return;
   }
 
+	//Get the stored regex from the user context
+	RegexStore* myRegStore = static_cast<RegexStore*>(sqlite3_user_data(context));
+
   u16string pattern = u16string((const char16_t*)sqlite3_value_text16(argv[0]));
   u16string in_string = u16string((const char16_t*)sqlite3_value_text16(argv[1]));
-  regex_t exp;
-  int err = regcomp(&exp, std::string(pattern.begin(), pattern.end()).c_str(), REG_EXTENDED);
-  //Return no results if the expression did not compile.
-  if (0 != err) {
-    sqlite3_result_error_code(context, 3);
-    return;
-  }
 
-  //Check each match to make sure it consumes the whole string
-  regmatch_t pmatch;
-  int match = regexec(&exp, std::string(in_string.begin(), in_string.end()).c_str(), 1, &pmatch, 0);
-  if (0 == match and 0 == pmatch.rm_so and in_string.size() == pmatch.rm_eo) {
-    sqlite3_result_int(context, true);
-  }
-  else {
-    sqlite3_result_int(context, false);
-  }
-  //TODO FIXME This should be stored in the context pointer and freed after attempting all of the matches.
-  regfree(&exp);
+	if (not myRegStore->preparePattern(pattern)) {
+		sqlite3_result_error_code(context, 3);
+	}
+	sqlite3_result_int(context, myRegStore->patternMatch(in_string));
   return;
 }
 
 int initializeRegex(sqlite3 *db) {
-	return sqlite3_create_function_v2(db, "REGEXP", 2, SQLITE_UTF16, NULL, sqlite3_regexp, NULL, NULL, NULL);
+	//TODO SQLITE_DETERMINISTIC was added in 3.8.3, should be ORed with SQLITE_UTF16
+	RegexStore* myRegStore = new RegexStore();
+	return sqlite3_create_function_v2(db, "REGEXP", 2, SQLITE_UTF16, myRegStore, sqlite3_regexp, NULL, NULL, xDestroy);
 }
 
